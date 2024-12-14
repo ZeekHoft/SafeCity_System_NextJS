009f5e716a20029f26c6a8b8d85cbb1782b2021e
Nero Hoya <dltposiedon@gmail.com>
2024-12-14 19:26:37 +0800

    able to send input data using url in description emergency

--- app/report_history/page.js
+++ app/report_history/page.js
@@ -1,7 +1,14 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+
+export default function ReportHistory(context) {
+  const params = context.searchParams
   return (
-    <h1>page</h1>
-  );
+    <>
+      <h1>hellooo </h1>
+      <h2>emergency in {params.name ? params.name : ''}</h2>
+
+    </>
+
+  )
 }
